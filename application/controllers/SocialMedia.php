4e583bd5b19b22bdcb5187ce69be908de0e85be6
Trisna Nugraha <trisna.nugraha123@gmail.com>
2022-07-01 10:22:37 +0700

    finalize project

--- application/controllers/SocialMedia.php
+++ application/controllers/SocialMedia.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class SocialMedia extends MY_Controller
+class Socialmedia extends MY_Controller
 {
 
     public function __construct()
